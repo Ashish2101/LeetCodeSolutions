fbdc2147d0333612bb6f0021e58f7fc3df9892e2
Ashish Mehta <53532963+Ashish2101@users.noreply.github.com>
2020-09-30 23:57:02 +0530

    Update 107BinaryTreeLevelOrderTraversalII.py

--- 107BinaryTreeLevelOrderTraversalII.py
+++ 107BinaryTreeLevelOrderTraversalII.py
@@ -12,8 +12,8 @@ class Solution:
             if root.left != None and root.left.left == None and root.left.right == None:
                 return root.left.val + self.sumOfLeftLeaves(root.right)
             else:
-                return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
-
+                x= self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
+                return x
 
 
 
